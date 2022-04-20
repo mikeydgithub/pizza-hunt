a1d7812e7c739384e40731e706e27e187be8899a
mikeydgithub <ampgzo2@gmail.com>
2022-04-20 14:47:45 -0400

    comment controller update

---
+++ controllers/comment-controller.js
@@ -0,0 +1,55 @@
+const { Comment, Pizza } = require('../models');
+
+const commentController = {
+    // add comment to pizza
+    addComment({ params, body }, res) {
+        console.log(body);
+        Comment.create(body)
+        .then(({_id}) => {
+            return Pizza.findOneAndUpdate(
+                // our comments section will require an id already made from the parameters of a pizza id
+                {_id: params.pizzaId},
+                // push will create a new comment and give an id
+                {$push: { comments: _id } },
+                // new: true will return the updated comment
+                { new: true}
+            );
+        })
+        // if there is no error create the database for pizza data otherwise error check
+        .then(dbPizzaData => {
+            if(!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id!'});
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.json(err));
+    },
+
+    //remove comment. only requires the params and not a body since we can remove it with just an id.
+    removeComment({ params }, res) {
+        Comment.findOneAndDelete({_id: params.commenId })
+        .then(deletedComment => {
+            if(!deletedComment) {
+                return res.status(404).json({message: 'No comment with this id!'});
+            }
+            return Pizza.findOneAndUpdate(
+                { _id: params.pizzaId },
+                // pull it out of the existing parameters from comments by their commentId
+                { $pull: { comments: params.commentId } },
+                // new: true will return the updated comment
+                { new: true }
+            )
+        })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+              res.status(404).json({ message: 'No pizza found with this id!' });
+              return;
+            }
+            res.json(dbPizzaData);
+          })
+          .catch(err => res.json(err));
+    }
+};
+
+module.exports = commentController
--- models/Pizza.js
+++ models/Pizza.js
@@ -2,22 +2,46 @@
 const { Schema, model } = require('mongoose');
 
 // create the schema for the model
-const PizzaSchema = new Schema({
-    pizzaName: {
-        type: String
+const PizzaSchema = new Schema(
+    {
+        pizzaName: {
+            type: String
+        },
+        createdBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        size: {
+            type: String,
+            defualt: 'Large'
+        },
+        toppings: [],
+        comments:[
+            {
+                // Data comes from Comment model. Parent and child refernce. The Child can be referenced in the parent but not the other way around.
+                type: Schema.Types.ObjectId,
+                // ref: 'Comment' tells the Pizza model which documents to search
+                ref: 'Comment'
+            }
+        ]
     },
-    createdBy: {
-        type: String
+    // tell the schema to use virtuals.
+    {
+    toJSON: {
+        virutals: true,
     },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    size: {
-        type: String,
-        defualt: 'Large'
-    },
-    toppings: []
+    // set id to false bcause this is a virtual that mongoose returns and we don't need it.
+    id: false
+    }
+);
+
+// get total count of comments and replies on retrieval
+// virtual properties
+PizzaSchema.vitual('comment').get(function() {
+    return this.comments.length;
 });
 
 //create the pizza model using PizzaSchema
